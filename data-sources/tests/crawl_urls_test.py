ac93dbb1d7ff80dc3d22da092d214dffffe0c7e5
srogers47 <srogers47@mail.naz.edu>
2021-06-04 00:20:08 -0400

    Exhausted solutions to extract news from The Atlantic website. At an impass trying to parse dynamic classes and placement of elements. Next commit will include crawl_url_test.py scraping The Economist.  Class and format are consitent.  Site uses xml article indexing similar to The Atlantic so previous tests will remain unchanged.

--- data-sources/tests/crawl_urls_test.py
+++ data-sources/tests/crawl_urls_test.py
@@ -2,16 +2,17 @@
 
 import asyncio
 import aiohttp
-import tqdm 
+#import tqdm 
 import lxml
 import json
 from bs4 import BeautifulSoup as BS 
-import re 
+
 
 
 class Main:
     sitemap_urls = [] #List of urls returned from gen_urls(). 
     article_urls = [] #Article urls extracted from sitemaps. 
+    articles_missing_text = 0 #Need to skip articles without article text to avoid disruptive type errors.
 
     def gen_urls(self):
         """
@@ -19,7 +20,7 @@ class Main:
         Will take a hardcoded base url as input and generate a set of urls as output.
         """ 
         base_url = 'https://www.theatlantic.com/sitemaps/sitemap_YEAR_MONTH.xml' #YEAR and MONTH are used as placeholders.  
-        #Nested iteration 
+        #Nested iteration for each month of each year.
         for y in range(2010,2021,1): #10 years 
             temp_url = base_url.replace('YEAR', str(y)) 
             for m in range(1,13): #12 months 
@@ -29,7 +30,6 @@ class Main:
                 else: 
                     new_url = temp_url.replace('MONTH', str(m)) 
                 self.sitemap_urls.append(new_url)
-                print(new_url) 
         return self.sitemap_urls
 
     async def fetch_url(self, session, url):
@@ -39,7 +39,6 @@ class Main:
         """ 
         async with session.get(url) as response:
             text = await response.text() #Need await in context manager async with 
-            #print("URL: {} - TEXT:I {}".format(url, len(text)))
             #Parse xml file for article urls 
             xml_soup = BS(text, "lxml")
             self.article_urls.append(xml_soup.loc.string) #NOTE: Due to async tasks completing without order, article urls won't be sorted. 
@@ -49,42 +48,53 @@ class Main:
 
 
     async def crawl_url(self, session, article_url):
-        """Crawl article urls and extract text/html.""" 
-        print(article_url) 
-        async with session.get(article_url) as response: #from article_urls
-            html = await response.text() 
-            print("THIS IS HTML ", html) 
-            soup = BS(html, "html.parser") #Make the soup, parse html to extract article data points.
-            #Selectors are dynamic (with exception to author), will change periodically.  
-            #Select via class containing desired text. 
-            content = soup.main.article
-            print("THIS IS CONTENT",content) 
+        """
+        Crawl article urls and extract datapoints.
+        Serialize data.
+        """ 
+        articles_json = {"Title":[], "Author":[], "Date/Time Posted":[], "Article Text":[]} #All articles will be serialized prior to database storage.  
+        article_text = [] #Article text extracted from a variable amount of <p> tags nested in seperate elements. 
+        #Check url route to discern artcles from photos and videos  
+        if ('/photo/' in article_url) or ('/video/' in article_url): 
+            print(f"{article_url} displays media with inefficient amount of text")
+            self.articles_missing_text += 1 #Will aid in calculating confidence (p-level) 
+            return self.articles_missing_text 
+        else:
+            async with session.get(article_url) as response: #from article_urls
+                html = await response.text()  
+                soup = BS(html, "lxml") #Make the soup, parse html to extract article data points.
+                
+                #Selectors are dynamic (with exception to author), will change per request/render.  
+                #Select via class containing desired text. 
+            
+                title = soup.select_one('h1[class^="ArticleHeader"]')
 
-            ###REGULAR EXPRESSIONS with css selectors 
+                print(title) 
+                author = soup.find("address", {'id'='byline'})
+                author = author.a 
+                date_posted = soup.header.time.attrs['datetime']  #Data posted for sorting events to compare to financial data.
+                
+                #All text is in <p> tags some nested inside <blockqoutes within <section class="ArticleContent..."      
+                text_container = soup.select_one('section[class^="ArticleContent"]') 
+                if text_container.p:
+                    for p in text_container.p:              #What if there are multiple blockquote
+                        article_text.append(p.text) 
+                    
+                if text_container.blockquote:
+                    for p in text_container.blockquote.p:
+                        article_text.append(p.text) 
 
-            title = content.select_one("h1[class^='ArticleTitle']").string
-            author = content.select_one("address[id='byline']") #Locate via id, NOT regex
-            author = author.a.string #Author nested in <a tag text
-            article_text = content.select_one("p[class^='ArticleParagraph']").string #All article text.
-            date_posted = content.select_one("time[class^='ArticleTimestamp']").attrs  #Data posted for sorting events to compare to financial data.
-
-
-            #NOTE: Prepare data for insert. Need to serialize data into JSON before inserting into mongodb database.
-            #First create a dict 
-            temp_dict = {
-                    "Title":title,
-                    "Author":author,
-                    "Text":article_text, #NOTE: DEBUG. Text in json is "null"? Fix extraction 
-                    "Date/time":str(date_posted) #Should i specify string? 
-                    } 
-            #Then serialize
-            json_article = json.dumps(temp_dict) 
-            print(json_article) 
-            return json_article 
-
-        #NOTE:Next test will look at async placement of json into non-relational database --> async_data_storage.py 
-
-
+                #Prepare data for insert. Need to serialize data into JSON before inserting into mongodb database.
+                #First create a dict 
+                #Then serialize
+                articles_dict["Title"].append(title)
+                articles_dict["Author"].append(author)
+                articles_dict["Date/Time Posted"].append(date_posted)
+                articles_dict["Article Text"].append(article_text) 
+                #In database_test.py code will save articles. This test soley concerns crawling article urls and extracting data points. 
+                json_article = json.dumps(articles_dict)
+                print(json_article) 
+                return json_article 
 
     async def parse_url(self, session, url): 
         """
@@ -100,7 +110,6 @@ class Main:
         async with aiohttp.ClientSession(loop=loop) as session:
             tasks = [self.parse_url(session, str(url)) for url in self.sitemap_urls] #Create tasks for getting article urls from sitemap_urls list. 
             responses = await asyncio.gather(*tasks) 
-            #responses = [await f for f in tqdm.tqdm(asyncio.as_completed(tasks), total = len(tasks))] 
             return responses 
 
 if __name__ == "__main__":
@@ -109,5 +118,5 @@ if __name__ == "__main__":
     m.gen_urls() 
     loop = asyncio.get_event_loop()
     parsed_data = loop.run_until_complete(m.parse_urls(m.sitemap_urls, loop)) 
-    print(m.article_urls) #NOTE: Debugging 
     print(parsed_data) 
+    print("Scraped", int(len(self.article_urls)) - int(self.articles_missing_text), "out of", len(self.article_urls)) #Debugging..how many articles get skipped over?
